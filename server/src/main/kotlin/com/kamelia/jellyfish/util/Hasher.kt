4ac22fcbc50bf7ee51ee4828767d93ad0f9768be
notKamui <teillard.jimmy@outlook.fr>
2022-05-15 19:31:40 +0200

    Upgraded hashing algorithm from SHA-256 to SHA-512

--- server/src/main/kotlin/com/kamelia/jellyfish/util/Hasher.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/util/Hasher.kt
@@ -4,8 +4,8 @@ import io.ktor.util.getDigestFunction
 
 object Hasher {
     private val salt = System.getenv("JELLYFISH_SALT") ?: ""
-    private val sha256 = getDigestFunction("SHA-256") { salt + it }
+    private val sha512 = getDigestFunction("SHA-512") { salt + it }
 
-    fun hash(password: String): String = sha256(password)
+    fun hash(password: String): String = sha512(password)
         .fold("") { str, it -> str + "%02x".format(it) }
 }

b056db74ee51e7208d01a274719fd9a1da20668c
notKamui <teillard.jimmy@outlook.fr>
2022-06-09 21:22:05 +0200

    Reworked Hasher to use bcrypt algorithm instead of SHA-512

--- server/src/main/kotlin/com/kamelia/jellyfish/util/Hasher.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/util/Hasher.kt
@@ -1,11 +1,20 @@
 package com.kamelia.jellyfish.util
 
-import io.ktor.util.getDigestFunction
+import at.favre.lib.crypto.bcrypt.BCrypt
+import at.favre.lib.crypto.bcrypt.LongPasswordStrategies
+import java.security.SecureRandom
 
 object Hasher {
-    private val salt = System.getenv("JELLYFISH_SALT") ?: ""
-    private val sha512 = getDigestFunction("SHA-512") { salt + it }
+    private val version = BCrypt.Version.VERSION_2B
+    private val strategy = LongPasswordStrategies.truncate(version)
+    private val hasher = BCrypt.with(version, SecureRandom(), strategy)
+    private val verifier = BCrypt.verifyer(version, strategy)
 
-    fun hash(password: String): String = sha512(password)
-        .fold("") { str, it -> str + "%02x".format(it) }
+    private const val strength = 6
+
+    fun hash(password: String, strength: Int = this.strength): String =
+        hasher.hashToString(strength, password.toCharArray())
+
+    fun verify(password: String, hash: String): BCrypt.Result =
+        verifier.verify(password.toCharArray(), hash.toCharArray())
 }

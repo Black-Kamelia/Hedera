b713c9c91630706e2746c8d71bd1b03426295ebd
Irwin Madet <irwin.madet@gmail.com>
2022-06-21 23:17:15 +0200

    User entity

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("org.jetbrains.exposed", "exposed-core", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-dao", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
+    implementation("org.jetbrains.exposed", "exposed-java-time", exposedVersion)
     implementation("org.postgresql", "postgresql", "42.3.6")
     implementation("com.zaxxer", "HikariCP", "5.0.1")
     implementation("at.favre.lib", "bcrypt", "0.9.0")

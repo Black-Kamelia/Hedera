47d9a1ae15c34b863f1421293fb0f3c5314556a0
Irwin Madet <irwin.madet@gmail.com>
2022-06-22 00:19:45 +0200

    Rework user service

--- server/src/main/kotlin/com/kamelia/jellyfish/rest/user/Service.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/rest/user/Service.kt
@@ -1,22 +1,26 @@
 package com.kamelia.jellyfish.rest.user
 
+import com.kamelia.jellyfish.util.ErrorDTO
 import com.kamelia.jellyfish.util.QueryResult
 import java.util.UUID
 
 object UserService {
 
-    suspend fun signup(dto: UserDTO): QueryResult<UserResponseDTO, String> {
+    suspend fun signup(dto: UserDTO): QueryResult<UserResponseDTO, List<ErrorDTO>> {
         Users.findByEmail(dto.email)?.let {
-            return QueryResult.forbidden("Email already exists")
+            return QueryResult.forbidden("errors.users.email.already_exists")
         }
         Users.findByUsername(dto.username)?.let {
-            return QueryResult.forbidden("Username already exists")
+            return QueryResult.forbidden("errors.users.username.already_exists")
+        }
+        if (dto.role != UserRole.REGULAR) {
+            return QueryResult.forbidden("errors.users.role.forbidden")
         }
         // TODO: check if email is valid, password valid, role elevation, etc
         return QueryResult.ok(Users.create(dto).toDTO())
     }
 
-    suspend fun delete(id: UUID): QueryResult<UserResponseDTO, Nothing> {
+    suspend fun deleteUser(id: UUID): QueryResult<UserResponseDTO, Nothing> {
         // TODO: check if user is admin, etc
         Users.delete(id)?.let {
             return QueryResult.ok(it.toDTO())

d42223f64218f68b3417491eea439add697c7a95
notKamui <teillard.jimmy@outlook.fr>
2022-06-22 12:50:50 +0200

    Added routes and service to update a user (to be reworked to use optional arguments)

--- server/src/main/kotlin/com/kamelia/jellyfish/rest/user/Routes.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/rest/user/Routes.kt
@@ -2,19 +2,20 @@ package com.kamelia.jellyfish.rest.user
 
 import com.kamelia.jellyfish.util.QueryResult
 import com.kamelia.jellyfish.util.respond
+import com.kamelia.jellyfish.util.toUUIDOrNull
 import io.ktor.server.application.call
 import io.ktor.server.request.receive
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.delete
 import io.ktor.server.routing.post
+import io.ktor.server.routing.put
 import io.ktor.server.routing.route
 import java.lang.AssertionError
-import java.lang.IllegalArgumentException
-import java.util.*
 
 
 fun Route.userRoutes() = route("/users") {
     signup()
+    updateUser()
     deleteUser()
 }
 
@@ -23,15 +24,19 @@ private fun Route.signup() = post {
     call.respond(UserService.signup(user))
 }
 
+private fun Route.updateUser() = put("/{uuid}") {
+    val id = call.parameters["uuid"] ?: throw AssertionError("Ktor is literally dying")
+    val update = call.receive<UserUpdateDTO>()
+    val uuid = id.toUUIDOrNull()
+        ?: return@put call.respond(QueryResult.badRequest("UUID is not valid"))
+
+    call.respond(UserService.updateUser(uuid, update))
+}
+
 private fun Route.deleteUser() = delete("/{uuid}") {
-    val uuid = call.parameters["uuid"] ?: throw AssertionError("Ktor is literally dying")
+    val id = call.parameters["uuid"] ?: throw AssertionError("Ktor is literally dying")
+    val uuid = id.toUUIDOrNull()
+        ?: return@delete call.respond(QueryResult.badRequest("UUID is not valid"))
 
-    val id = try {
-        UUID.fromString(uuid)
-    } catch (e: IllegalArgumentException) {
-        call.respond(QueryResult.badRequest("UUID is not valid"))
-        return@delete
-    }
-
-    call.respond(UserService.deleteUser(id))
+    call.respond(UserService.deleteUser(uuid))
 }

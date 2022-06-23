49188e15b904e7b55df0b24fb62edf317314f3e8
notKamui <teillard.jimmy@outlook.fr>
2022-06-23 21:26:34 +0200

    Removed unnecessary hook

--- server/src/main/kotlin/com/kamelia/jellyfish/rest/core/auditable/Entity.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/rest/core/auditable/Entity.kt
@@ -1,17 +1,12 @@
 package com.kamelia.jellyfish.rest.core.auditable
 
 import com.kamelia.jellyfish.rest.user.User
-import com.kamelia.jellyfish.rest.user.User.Companion.optionalReferrersOn
 import com.kamelia.jellyfish.rest.user.Users
 import java.time.Instant
 import java.util.UUID
-import org.jetbrains.exposed.dao.EntityChangeType
-import org.jetbrains.exposed.dao.EntityHook
 import org.jetbrains.exposed.dao.UUIDEntity
-import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.UUIDTable
-import org.jetbrains.exposed.dao.toEntity
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.javatime.timestamp
 
@@ -23,11 +18,18 @@ abstract class AuditableUUIDTable(name: String) : UUIDTable(name) {
 }
 
 abstract class AuditableUUIDEntity(id: EntityID<UUID>, table: AuditableUUIDTable) : UUIDEntity(id) {
-    @Suppress("unused")
-    var createdAt by table.createdAt // automatically set, never touched
+    @Suppress("unused") // automatically set, never touched
+    var createdAt by table.createdAt
+        private set
+
     var createdBy by User referencedOn table.createdBy
+        private set
+
     var updatedAt by table.updatedAt
+        private set
+
     var updatedBy by User optionalReferencedOn table.updatedBy
+        private set
 
     fun onCreate(creator: User) {
         createdBy = creator
@@ -35,17 +37,6 @@ abstract class AuditableUUIDEntity(id: EntityID<UUID>, table: AuditableUUIDTable
 
     fun onUpdate(updater: User) {
         updatedBy = updater
-    }
-}
-
-abstract class AuditableUUIDEntityClass<E : AuditableUUIDEntity>(
-    table: AuditableUUIDTable
-) : UUIDEntityClass<E>(table) {
-
-    init {
-        EntityHook.subscribe { change ->
-            if (change.changeType != EntityChangeType.Updated) return@subscribe
-            change.toEntity(this)?.updatedAt = Instant.now()
-        }
+        updatedAt = Instant.now()
     }
 }

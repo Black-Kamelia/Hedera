2bd5c8b9ca48700608c5466886d9552bc1a909b3
notKamui <teillard.jimmy@outlook.fr>
2022-06-24 20:47:50 +0200

    Linked the ExceptionAdvisor API to the routes

--- server/src/main/kotlin/com/kamelia/jellyfish/util/Extensions.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/util/Extensions.kt
@@ -1,8 +1,16 @@
 package com.kamelia.jellyfish.util
 
-import java.lang.IllegalArgumentException
+import com.kamelia.jellyfish.core.InvalidUUIDException
+import com.kamelia.jellyfish.core.MissingParameterException
+import io.ktor.server.application.ApplicationCall
 import java.util.UUID
 import org.jetbrains.exposed.dao.UUIDEntity
+
+fun ApplicationCall.getParamOrNull(name: String): String? =
+    parameters[name]
+
+fun ApplicationCall.getParam(name: String): String =
+    getParamOrNull(name) ?: throw MissingParameterException(name)
 
 fun String.toUUIDOrNull() = try {
     UUID.fromString(this)
@@ -12,3 +20,9 @@ fun String.toUUIDOrNull() = try {
 
 val UUIDEntity.uuid: UUID
     get() = id.value
+
+fun ApplicationCall.getUUIDOrNull(): UUID? =
+    getParamOrNull("uuid")?.toUUIDOrNull()
+
+fun ApplicationCall.getUUID(): UUID =
+    getUUIDOrNull() ?: throw InvalidUUIDException()

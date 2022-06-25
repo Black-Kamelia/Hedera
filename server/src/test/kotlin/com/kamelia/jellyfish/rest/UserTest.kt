f4fc90a9a4da35e622391383f07690cae720072f
Irwin Madet <irwin.madet@gmail.com>
2022-06-25 17:58:19 +0200

    Add some new tests

--- server/src/test/kotlin/com/kamelia/jellyfish/rest/UserTest.kt
+++ server/src/test/kotlin/com/kamelia/jellyfish/rest/UserTest.kt
@@ -1,10 +1,13 @@
 package com.kamelia.jellyfish.rest
 
 import com.kamelia.jellyfish.rest.user.UserDTO
+import com.kamelia.jellyfish.rest.user.UserPasswordUpdateDTO
 import com.kamelia.jellyfish.rest.user.UserResponseDTO
 import com.kamelia.jellyfish.rest.user.UserRole
+import com.kamelia.jellyfish.rest.user.UserUpdateDTO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.delete
+import io.ktor.client.request.patch
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
@@ -14,9 +17,11 @@ import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.testing.ApplicationTestBuilder
 import io.ktor.server.testing.testApplication
+import java.util.*
 import kotlinx.serialization.json.Json
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 fun ApplicationTestBuilder.client() = createClient {
     install(ContentNegotiation) {
@@ -27,7 +32,7 @@ fun ApplicationTestBuilder.client() = createClient {
 class UserTest {
 
     @Test
-    fun `Signing up`() = testApplication {
+    fun `Sign up`() = testApplication {
         val newUserDto = UserDTO(
             username = "test",
             password = "test",
@@ -49,11 +54,11 @@ class UserTest {
     }
 
     @Test
-    fun `Signing up with existing email`() = testApplication {
+    fun `Sign up with existing email`() = testApplication {
         val dto = UserDTO(
             username = "test",
             password = "test",
-            email = "admin@admin.com"
+            email = "admin@test.com"
         )
         val response = client().post("/api/users") {
             contentType(ContentType.Application.Json)
@@ -63,7 +68,7 @@ class UserTest {
     }
 
     @Test
-    fun `Signing up with existing username`() = testApplication {
+    fun `Sign up with existing username`() = testApplication {
         val dto = UserDTO(
             username = "admin",
             password = "test",
@@ -74,6 +79,90 @@ class UserTest {
             setBody(dto)
         }
         assertEquals(HttpStatusCode.Forbidden, response.status)
+    }
+
+    @Test
+    fun `Update username`() = testApplication {
+        val client = client()
+        val response = client.patch("/api/users/00000000-0000-0000-0000-000000000001") {
+            contentType(ContentType.Application.Json)
+            setBody(
+                UserUpdateDTO(
+                    username = "newUsername"
+                )
+            )
+        }
+        assertEquals(HttpStatusCode.OK, response.status)
+        val responseDto = Json.decodeFromString(UserResponseDTO.serializer(), response.bodyAsText())
+        assertEquals(UUID.fromString("00000000-0000-0000-0000-000000000001"), responseDto.id)
+        assertEquals("newUsername", responseDto.username)
+        assertEquals("user1@test.com", responseDto.email)
+        assertEquals(UserRole.REGULAR, responseDto.role)
+        assertTrue { responseDto.enabled }
+    }
+
+    @Test
+    fun `Update email address`() = testApplication {
+        val client = client()
+        val response = client.patch("/api/users/00000000-0000-0000-0000-000000000002") {
+            contentType(ContentType.Application.Json)
+            setBody(
+                UserUpdateDTO(
+                    email = "newEmail@test.com"
+                )
+            )
+        }
+        assertEquals(HttpStatusCode.OK, response.status)
+        val responseDto = Json.decodeFromString(UserResponseDTO.serializer(), response.bodyAsText())
+        assertEquals(UUID.fromString("00000000-0000-0000-0000-000000000002"), responseDto.id)
+        assertEquals("user2", responseDto.username)
+        assertEquals("newEmail@test.com", responseDto.email)
+        assertEquals(UserRole.REGULAR, responseDto.role)
+        assertTrue { responseDto.enabled }
+    }
+
+    @Test
+    fun `Update password with correct old password`() = testApplication {
+        val client = client()
+        val response = client.patch("/api/users/00000000-0000-0000-0000-000000000001/password") {
+            contentType(ContentType.Application.Json)
+            setBody(
+                UserPasswordUpdateDTO(
+                    "password",
+                    "newPassword"
+                )
+            )
+        }
+        assertEquals(HttpStatusCode.OK, response.status)
+    }
+
+    @Test
+    fun `Update password with wrong old password`() = testApplication {
+        val client = client()
+        val response = client.patch("/api/users/00000000-0000-0000-0000-000000000002/password") {
+            contentType(ContentType.Application.Json)
+            setBody(
+                UserPasswordUpdateDTO(
+                    "wrongPassword",
+                    "newPassword"
+                )
+            )
+        }
+        assertEquals(HttpStatusCode.Forbidden, response.status)
+    }
+
+    @Test
+    fun `Update unknown user`() = testApplication {
+        val client = client()
+        val response = client.patch("/api/users/00000000-0000-0000-0000-00000000000f") {
+            contentType(ContentType.Application.Json)
+            setBody(
+                UserUpdateDTO(
+                    email = "newEmail@test.com"
+                )
+            )
+        }
+        assertEquals(HttpStatusCode.NotFound, response.status)
     }
 
     @Test

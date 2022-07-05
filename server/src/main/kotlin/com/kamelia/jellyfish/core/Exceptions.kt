7f7d52983fdc809deb3dd4b71cd893b610b17294
notKamui <teillard.jimmy@outlook.fr>
2022-07-06 00:04:55 +0200

    Added new exception and advisor for invalid tokens

--- server/src/main/kotlin/com/kamelia/jellyfish/core/Exceptions.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/core/Exceptions.kt
@@ -3,3 +3,5 @@ package com.kamelia.jellyfish.core
 class InvalidUUIDException : Exception("Query param 'uuid' is malformed")
 
 class MissingParameterException(name: String) : Exception("Query param '$name' is missing")
+
+class ExpiredOrInvalidTokenException : Exception("Token is expired or invalid")

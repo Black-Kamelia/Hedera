aecd17caf7a04ce804535f092207b5a22619ae26
notKamui <teillard.jimmy@outlook.fr>
2022-07-06 00:29:29 +0200

    Remove redirection on invalid token

--- server/src/main/kotlin/com/kamelia/jellyfish/plugins/Authentication.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/plugins/Authentication.kt
@@ -4,14 +4,11 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.kamelia.jellyfish.rest.user.Users
 import com.kamelia.jellyfish.util.Environment
-import com.kamelia.jellyfish.util.Environment.isDev
-import com.kamelia.jellyfish.util.Environment.isProd
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.auth.Authentication
 import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.jwt.jwt
-import io.ktor.server.response.respondRedirect
 
 fun Application.configureAuthentication() {
     install(Authentication) {
@@ -29,13 +26,6 @@ fun Application.configureAuthentication() {
                     return@validate null
                 }
                 JWTPrincipal(credential.payload)
-            }
-            
-            challenge {_, _ ->
-                when {
-                    isDev -> call.respondRedirect("http://localhost:3000/login")
-                    isProd -> call.respondRedirect("/login")
-                }
             }
         }
     }

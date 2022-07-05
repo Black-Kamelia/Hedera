b173ab901c38d38c616c69459c9b65edeb75b8c1
notKamui <teillard.jimmy@outlook.fr>
2022-07-06 00:04:55 +0200

    Added new exception and advisor for invalid tokens

--- server/src/main/kotlin/com/kamelia/jellyfish/core/ExceptionAdvisors.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/core/ExceptionAdvisors.kt
@@ -8,21 +8,22 @@ import kotlinx.serialization.SerializationException
 private suspend fun badRequestMessage(e: Throwable, call: ApplicationCall) =
     call.respond(QueryResult.badRequest(e.message!!))
 
-val MissingParameterAdvisor =
-    exceptionAdvisor<MissingParameterException>(::badRequestMessage)
+val MissingParameterAdvisor = exceptionAdvisor<MissingParameterException>(::badRequestMessage)
 
-val IllegalArgumentAdvisor =
-    exceptionAdvisor<IllegalArgumentException>(::badRequestMessage)
+val IllegalArgumentAdvisor = exceptionAdvisor<IllegalArgumentException>(::badRequestMessage)
 
-val InvalidUUIDAdvisor =
-    exceptionAdvisor<InvalidUUIDException>(::badRequestMessage)
+val InvalidUUIDAdvisor = exceptionAdvisor<InvalidUUIDException>(::badRequestMessage)
 
-val SerializationAdvisor =
-    exceptionAdvisor<SerializationException>(::badRequestMessage)
+val SerializationAdvisor = exceptionAdvisor<SerializationException>(::badRequestMessage)
+
+val ExpiredOrInvalidTokenAdvisor = exceptionAdvisor<ExpiredOrInvalidTokenException> { e, call ->
+    call.respond(QueryResult.unauthorized(e.message!!))
+}
 
 val BasicAdvisor = arrayOf(
     MissingParameterAdvisor,
     IllegalArgumentAdvisor,
     InvalidUUIDAdvisor,
     SerializationAdvisor,
+    ExpiredOrInvalidTokenAdvisor
 )

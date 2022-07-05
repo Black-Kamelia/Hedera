0d9f395a23aebb5b804b85afb08e99167989bcbd
notKamui <teillard.jimmy@outlook.fr>
2022-07-06 00:16:43 +0200

    Restricted user routes

--- server/src/main/kotlin/com/kamelia/jellyfish/rest/user/Routes.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/rest/user/Routes.kt
@@ -4,7 +4,10 @@ import com.kamelia.jellyfish.core.deleteOrCatch
 import com.kamelia.jellyfish.core.getOrCatch
 import com.kamelia.jellyfish.core.patchOrCatch
 import com.kamelia.jellyfish.core.postOrCatch
+import com.kamelia.jellyfish.util.adminRestrict
 import com.kamelia.jellyfish.util.getUUID
+import com.kamelia.jellyfish.util.idRestrict
+import com.kamelia.jellyfish.util.ifRegular
 import com.kamelia.jellyfish.util.respond
 import io.ktor.server.application.call
 import io.ktor.server.auth.authenticate
@@ -36,18 +39,22 @@ private fun Route.login() = postOrCatch<UserLoginDTO> { body ->
 
 private fun Route.getUserById() = getOrCatch(path = "/{uuid}") {
     val uuid = call.getUUID()
+    ifRegular { idRestrict(uuid) }
     call.respond(UserService.getUserById(uuid))
 }
 
 private fun Route.getAllUsers() = getOrCatch(path = "/all") {
+    adminRestrict()
     call.respond(UserService.getUsers())
 }
 
 private fun Route.getPagedUsers() = getOrCatch {
-    val page = (call.request.queryParameters["page"] ?: "0").let {
+    adminRestrict()
+    val params = call.request.queryParameters
+    val page = (params["page"] ?: "0").let {
         it.toLongOrNull() ?: throw IllegalArgumentException("Invalid page number")
     }
-    val pageSize = (call.request.queryParameters["pageSize"] ?: "25").let {
+    val pageSize = (params["pageSize"] ?: "25").let {
         it.toIntOrNull() ?: throw IllegalArgumentException("Invalid page size")
     }
     call.respond(UserService.getUsers(page, pageSize))
@@ -55,15 +62,18 @@ private fun Route.getPagedUsers() = getOrCatch {
 
 private fun Route.updateUser() = patchOrCatch<UserUpdateDTO>(path = "/{uuid}") { body ->
     val uuid = call.getUUID()
+    ifRegular { idRestrict(uuid) }
     call.respond(UserService.updateUser(uuid, body))
 }
 
 private fun Route.updateUserPassword() = patchOrCatch<UserPasswordUpdateDTO>(path = "/{uuid}/password") { body ->
     val uuid = call.getUUID()
+    idRestrict(uuid)
     call.respond(UserService.updateUserPassword(uuid, body))
 }
 
 private fun Route.deleteUser() = deleteOrCatch(path = "/{uuid}") {
     val uuid = call.getUUID()
+    ifRegular { idRestrict(uuid) }
     call.respond(UserService.deleteUser(uuid))
 }

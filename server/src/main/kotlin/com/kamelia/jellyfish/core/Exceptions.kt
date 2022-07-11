7aee0abc82034c08d50b404ccc27c5dd329120b3
Irwin Madet <irwin.madet@gmail.com>
2022-07-11 23:54:09 +0200

    File uploading support (with JWT or upload token)

--- server/src/main/kotlin/com/kamelia/jellyfish/core/Exceptions.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/core/Exceptions.kt
@@ -5,3 +5,5 @@ class InvalidUUIDException : Exception("Query param 'uuid' is malformed")
 class MissingParameterException(name: String) : Exception("Query param '$name' is missing")
 
 class ExpiredOrInvalidTokenException : Exception("errors.tokens.expired_or_invalid")
+
+class UploadCodeGenerationException : Exception("errors.uploads.code_generation")
---
+++ server/src/main/kotlin/com/kamelia/jellyfish/rest/file/Routes.kt
@@ -0,0 +1,53 @@
+package com.kamelia.jellyfish.rest.file
+
+import com.kamelia.jellyfish.core.ExpiredOrInvalidTokenException
+import com.kamelia.jellyfish.core.postOrCatch
+import com.kamelia.jellyfish.rest.user.Users
+import com.kamelia.jellyfish.util.QueryResult
+import com.kamelia.jellyfish.util.get
+import com.kamelia.jellyfish.util.jwt
+import com.kamelia.jellyfish.util.respond
+import com.kamelia.jellyfish.util.toUUIDOrNull
+import io.ktor.http.content.PartData
+import io.ktor.http.content.forEachPart
+import io.ktor.server.application.call
+import io.ktor.server.auth.authenticate
+import io.ktor.server.request.receiveMultipart
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.route
+
+fun Route.filesRoutes() = route("/files") {
+    uploadFileFromToken()
+
+    authenticate("auth-jwt") {
+        uploadFile()
+    }
+}
+
+private fun Route.uploadFile() = postOrCatch(path = "/upload") {
+    val uuid = jwt["id"].asString().toUUIDOrNull()!!
+    val user = Users.findById(uuid) ?: throw ExpiredOrInvalidTokenException()
+
+    call.receiveMultipart().forEachPart { part ->
+        if (part is PartData.FileItem && part.name == "file") {
+            call.respond(FileService.handleFile(part, user))
+        } else {
+            part.dispose()
+        }
+    }
+}
+
+private fun Route.uploadFileFromToken() = postOrCatch(path = "/upload/token") {
+    val authToken = call.request.headers["Upload-Token"]
+        ?: return@postOrCatch call.respond(QueryResult.badRequest("errors.upload.no_token"))
+
+    val user = Users.findByUploadToken(authToken) ?: throw ExpiredOrInvalidTokenException()
+
+    call.receiveMultipart().forEachPart { part ->
+        if (part is PartData.FileItem && part.name == "file") {
+            call.respond(FileService.handleFile(part, user))
+        } else {
+            part.dispose()
+        }
+    }
+}

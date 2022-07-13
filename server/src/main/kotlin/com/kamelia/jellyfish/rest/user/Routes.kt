91f2c2071fd5a09a35d55250cb3bb7d5ca135ab1
Irwin Madet <irwin.madet@gmail.com>
2022-07-13 22:55:55 +0200

    Extension to gather pagination parameters

--- server/src/main/kotlin/com/kamelia/jellyfish/rest/user/Routes.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/rest/user/Routes.kt
@@ -7,6 +7,7 @@ import com.kamelia.jellyfish.core.postOrCatch
 import com.kamelia.jellyfish.core.putOrCatch
 import com.kamelia.jellyfish.util.adminRestrict
 import com.kamelia.jellyfish.util.get
+import com.kamelia.jellyfish.util.getPageParameters
 import com.kamelia.jellyfish.util.getUUID
 import com.kamelia.jellyfish.util.idRestrict
 import com.kamelia.jellyfish.util.ifNotRegular
@@ -51,13 +52,7 @@ private fun Route.getAllUsers() = getOrCatch(path = "/all") {
 
 private fun Route.getPagedUsers() = getOrCatch {
     adminRestrict()
-    val params = call.request.queryParameters
-    val page = (params["page"] ?: "0").let {
-        it.toLongOrNull() ?: throw IllegalArgumentException("Invalid page number")
-    }
-    val pageSize = (params["pageSize"] ?: "25").let {
-        it.toIntOrNull() ?: throw IllegalArgumentException("Invalid page size")
-    }
+    val (page, pageSize) = call.getPageParameters()
     call.respond(UserService.getUsers(page, pageSize))
 }
 

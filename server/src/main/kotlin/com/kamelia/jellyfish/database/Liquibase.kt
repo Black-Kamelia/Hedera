36ef570cb07065467b40531f65ef9905a5be36c0
notKamui <teillard.jimmy@outlook.fr>
2022-07-23 23:03:06 +0200

    Refactor

--- server/src/main/kotlin/com/kamelia/jellyfish/database/Liquibase.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/database/Liquibase.kt
@@ -1,14 +1,13 @@
 package com.kamelia.jellyfish.database
 
 import com.kamelia.jellyfish.util.Environment.liquibaseMaster
-import io.ktor.server.application.Application
 import liquibase.Contexts
 import liquibase.Liquibase
 import liquibase.database.DatabaseFactory
 import liquibase.database.jvm.JdbcConnection
 import liquibase.resource.ClassLoaderResourceAccessor
 
-fun Application.configureLiquibase() {
+fun configureLiquibase() {
     // Establish connection to the database
     val jdbcConnection = JdbcConnection(Connection.connection)
     val database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(jdbcConnection)

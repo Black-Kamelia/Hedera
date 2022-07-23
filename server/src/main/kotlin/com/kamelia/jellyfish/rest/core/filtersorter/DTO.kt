5ce9a26a2abf117875ea47f48029415b075c839b
notKamui <teillard.jimmy@outlook.fr>
2022-07-23 22:38:34 +0200

    Added Filter and Sort DTOs

--- server/src/main/kotlin/com/kamelia/jellyfish/rest/core/filtersorter/DTO.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/rest/core/filtersorter/DTO.kt
@@ -1,37 +1,37 @@
 @file:UseSerializers(UUIDSerializer::class)
 
-package com.kamelia.jellyfish.rest.core.filter
+package com.kamelia.jellyfish.rest.core.filtersorter
 
+import com.kamelia.jellyfish.rest.core.DTO
 import com.kamelia.jellyfish.util.UUIDSerializer
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.UseSerializers
 
 
-@Serializable
+@kotlinx.serialization.Serializable
 data class FilterSorterDefinitionDTO(
-    val filter: FilterDefinitionDTO = listOf(),
+    val filters: FilterDefinitionDTO = listOf(),
     val sorter: SorterDefinitionDTO = listOf(),
-)
+) : DTO
 
 typealias FilterDefinitionDTO = List<FilterGroupDTO>
 
 typealias FilterGroupDTO = List<FilterObject>
 
-@Serializable
+@kotlinx.serialization.Serializable
 data class FilterObject(
     val field: String,
     val operator: String,
     val value: String,
     val type: FilterType = FilterType.POS,
-)
+) : DTO
 
 typealias SorterDefinitionDTO = List<SortObject>
 
-@Serializable
+@kotlinx.serialization.Serializable
 data class SortObject(
     val field: String,
     val direction: SortDirection = SortDirection.ASC,
-)
+) : DTO
 
 enum class FilterType { POS, NEG }
 enum class SortDirection { ASC, DES }

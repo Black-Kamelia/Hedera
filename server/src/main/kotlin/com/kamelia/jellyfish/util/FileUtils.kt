a36634d8cee4383077144a8eb43d7588d6339e5d
notKamui <teillard.jimmy@outlook.fr>
2022-07-31 00:54:14 +0200

    Added documentation

--- server/src/main/kotlin/com/kamelia/jellyfish/util/FileUtils.kt
+++ server/src/main/kotlin/com/kamelia/jellyfish/util/FileUtils.kt
@@ -18,11 +18,29 @@ object FileUtils {
         java.nio.file.Files.createDirectories(UPLOAD_PATH)
     }
 
+    /**
+     * Retrieves the file with the given [code] and [owner] from the disk, or null.
+     *
+     * @param code The code of the file to retrieve.
+     * @param owner The owner of the file to retrieve.
+     *
+     * @return The file with the given [code] and [owner] or null.
+     */
     fun getOrNull(owner: UUID, code: String): File? {
         val file = UPLOAD_PATH.resolve(owner.toString()).resolve(code).toFile()
         return if (file.exists()) file else null
     }
 
+    /**
+     * Writes the content of the [file] to the disk.
+     * The file name on the disk is generated with a unique code.
+     * The file will then be written in `[UPLOAD_PATH]/[owner]/[code]`.
+     *
+     * @param file the file to write to the disk
+     * @param owner the owner of the file
+     *
+     * @return the code of the file, the mime-type of the file, and the size of the file in bytes
+     */
     suspend fun write(
         owner: UUID,
         file: PartData.FileItem
@@ -38,6 +56,12 @@ object FileUtils {
         Triple(fileCode, fileMimeType, fileSize)
     }
 
+    /**
+     * Deletes the file with the given [code] and [owner] from the disk.
+     *
+     * @param owner the owner of the file
+     * @param code the code of the file
+     */
     fun delete(owner: UUID, code: String) {
         val file = UPLOAD_PATH.resolve(owner.toString()).resolve(code).toFile()
         if (file.exists()) file.delete()

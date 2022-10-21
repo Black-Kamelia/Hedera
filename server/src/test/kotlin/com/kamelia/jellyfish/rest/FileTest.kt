9cb5bbb139b4ba1660443a67257af4d7cb820720
Irwin Madet <irwin.madet@gmail.com>
2022-10-22 01:11:21 +0200

    Rework tests as parameterized tests (WIP)

--- server/src/test/kotlin/com/kamelia/jellyfish/rest/FileTest.kt
+++ server/src/test/kotlin/com/kamelia/jellyfish/rest/FileTest.kt
@@ -2,6 +2,7 @@ package com.kamelia.jellyfish.rest
 
 import com.kamelia.jellyfish.appendFile
 import com.kamelia.jellyfish.client
+import com.kamelia.jellyfish.core.TokenPair
 import com.kamelia.jellyfish.login
 import com.kamelia.jellyfish.rest.core.pageable.FilterObject
 import com.kamelia.jellyfish.rest.core.pageable.PageDefinitionDTO
@@ -23,49 +24,151 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import io.ktor.server.testing.testApplication
 import java.util.UUID
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
+import java.util.stream.Stream
 import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Named
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestMethodOrder
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-private lateinit var code: String
+typealias TestUser = Pair<TokenPair?, UUID>
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FileTest {
 
-    @Test
+    companion object {
+
+        private val fileCodes: MutableMap<UUID, String> = mutableMapOf()
+
+        private lateinit var user1: TestUser
+        private lateinit var user2: TestUser
+        private val guest: TestUser = Pair(null, UUID(0, 0))
+
+        init {
+            testApplication {
+                user1 = Pair(
+                    login("user1", "password").second ?: throw Exception("Login failed"),
+                    UUID.fromString("00000000-0000-0000-0000-000000000001")
+                )
+                user2 = Pair(
+                    login("user2", "password").second ?: throw Exception("Login failed"),
+                    UUID.fromString("00000000-0000-0000-0000-000000000002")
+                )
+            }
+        }
+
+        @JvmStatic
+        fun uploadFile(): Stream<Arguments> = Stream.of(
+            Arguments.of(Named.of("regular user 1", user1), HttpStatusCode.OK),
+            Arguments.of(Named.of("regular user 2", user2), HttpStatusCode.OK),
+            Arguments.of(Named.of("a guest", guest), HttpStatusCode.Unauthorized),
+        )
+
+        @JvmStatic
+        fun editFile(): Stream<Arguments> = Stream.of(
+            Arguments.of(
+                Named.of("the file owner", user1),
+                UUID.fromString("00000000-0000-0000-0002-000000000001"),
+                HttpStatusCode.OK
+            ),
+            Arguments.of(
+                Named.of("another user", user2),
+                UUID.fromString("00000000-0000-0000-0002-000000000002"),
+                HttpStatusCode.Forbidden
+            ),
+            Arguments.of(
+                Named.of("a guest", guest),
+                UUID.fromString("00000000-0000-0000-0000-000000000000"),
+                HttpStatusCode.Unauthorized
+            ),
+        )
+
+        @JvmStatic
+        fun deleteFile(): Stream<Arguments> = Stream.of(
+            Arguments.of(
+                Named.of("the file owner", user1),
+                UUID.fromString("00000000-0000-0000-0004-000000000001"),
+                HttpStatusCode.OK
+            ),
+            Arguments.of(
+                Named.of("another user", user2),
+                UUID.fromString("00000000-0000-0000-0004-000000000002"),
+                HttpStatusCode.Forbidden
+            ),
+            Arguments.of(
+                Named.of("a guest", guest),
+                UUID.fromString("00000000-0000-0000-0000-000000000000"),
+                HttpStatusCode.Unauthorized
+            ),
+        )
+
+        @JvmStatic
+        fun downloadPrivateFile(): Stream<Arguments> = Stream.of(
+            Arguments.of(
+                Named.of("the file owner", user1),
+                fileCodes[user1.second],
+                HttpStatusCode.OK
+            ),
+            Arguments.of(
+                Named.of("another user", user2),
+                fileCodes[user1.second],
+                HttpStatusCode.NotFound
+            ),
+            Arguments.of(
+                Named.of("a guest", guest),
+                fileCodes[user1.second],
+                HttpStatusCode.NotFound
+            ),
+        )
+    }
+
+    @DisplayName("Uploading a file")
+    @ParameterizedTest(name = "as {0} is {1}")
+    @MethodSource
     @Order(1)
-    fun `Upload image`() = testApplication {
-        val (status, tokens) = login("user1", "password")
-        assertEquals(HttpStatusCode.OK, status)
+    fun uploadFile(user: TestUser, statusCode: HttpStatusCode) = testApplication {
+        val (tokens, userId) = user
         val client = client()
         val response = client.submitFormWithBinaryData("/api/files/upload", formData {
             appendFile("/test_files/test.txt", "test.txt", "text/plain")
         }) {
-            bearerAuth(tokens!!.token)
+            tokens?.let {
+                bearerAuth(it.token)
+            }
         }
-        assertEquals(HttpStatusCode.OK, response.status)
-        val responseDto = Json.decodeFromString(FileRepresentationDTO.serializer(), response.bodyAsText())
-        assertEquals(UUID.fromString("00000000-0000-0000-0000-000000000001"), responseDto.ownerId)
-        assertEquals("text/plain", responseDto.mimeType)
-        code = responseDto.code
+        assertEquals(statusCode, response.status)
+        if (response.status == HttpStatusCode.OK) {
+            val responseDto = Json.decodeFromString(FileRepresentationDTO.serializer(), response.bodyAsText())
+            assertEquals(userId, responseDto.ownerId)
+            assertEquals("text/plain", responseDto.mimeType)
+            fileCodes[userId] = responseDto.code
+        }
     }
 
-    @Test
+    @DisplayName("Editing a file")
+    @ParameterizedTest(name = "as {0} is {2}")
+    @MethodSource
     @Order(2)
-    fun `Edit file`() = testApplication {
-        val (status, tokens) = login("user1", "password")
-        assertEquals(HttpStatusCode.OK, status)
+    fun editFile(user: TestUser, fileId: UUID, statusCode: HttpStatusCode) = testApplication {
+        val (tokens, _) = user
         val client = client()
-        val response = client.patch("/api/files/00000000-0000-0000-0000-000000000001") {
+        val response = client.patch("/api/files/$fileId") {
             contentType(ContentType.Application.Json)
             setBody(FileUpdateDTO(name = "bar.txt"))
-            bearerAuth(tokens!!.token)
+            tokens?.let {
+                bearerAuth(it.token)
+            }
         }
-        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals(statusCode, response.status)
     }
 
     @Test
@@ -88,29 +191,37 @@ class FileTest {
         assertEquals("bar.txt", bar)
     }
 
-    @Test
+    @DisplayName("Deleting a file")
+    @ParameterizedTest(name = "as {0} is {2}")
+    @MethodSource
     @Order(4)
-    fun `Delete file`() = testApplication {
-        val (status, tokens) = login("user1", "password")
-        assertEquals(HttpStatusCode.OK, status)
+    fun deleteFile(user: TestUser, fileId: UUID, statusCode: HttpStatusCode) = testApplication {
+        val (tokens, _) = user
         val client = client()
-        val response = client.delete("/api/files/00000000-0000-0000-0000-000000000000") {
-            bearerAuth(tokens!!.token)
+        val response = client.delete("/api/files/$fileId") {
+            tokens?.let {
+                bearerAuth(it.token)
+            }
         }
-        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals(statusCode, response.status)
     }
 
-    @Test
+    @DisplayName("Downloading a private file")
+    @ParameterizedTest(name = "as {0} is {2}")
+    @MethodSource
     @Order(5)
-    fun `Download file`() = testApplication {
-        val (status, tokens) = login("user1", "password")
-        assertEquals(HttpStatusCode.OK, status)
+    fun downloadPrivateFile(user: TestUser, fileCode: String, statusCode: HttpStatusCode) = testApplication {
+        val (tokens, _) = user
         val client = client()
-        val response = client.get("/api/files/$code") {
-            bearerAuth(tokens!!.token)
+        val response = client.get("/api/files/$fileCode") {
+            tokens?.let {
+                bearerAuth(it.token)
+            }
         }
-        assertEquals(HttpStatusCode.OK, response.status)
-        assertEquals(this::class.java.getResource("/test_files/test.txt")!!.readText(), response.bodyAsText())
+        assertEquals(statusCode, response.status)
+        if (response.status == HttpStatusCode.OK) {
+            assertEquals(this::class.java.getResource("/test_files/test.txt")!!.readText(), response.bodyAsText())
+        }
     }
 
     @Test

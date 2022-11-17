ed4ed7bc515291c5f230e1b76a87fc7fb74f9509
Irwin Madet <irwin.madet@gmail.com>
2022-11-17 23:45:33 +0100

    Add test to check Bcrypt behaviors on very long yet not identical strings

--- server/src/test/kotlin/com/kamelia/jellyfish/util/HasherTest.kt
+++ server/src/test/kotlin/com/kamelia/jellyfish/util/HasherTest.kt
@@ -3,7 +3,8 @@ package com.kamelia.jellyfish.util
 import com.kamelia.jellyfish.core.Hasher
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
-import org.junit.Test
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 
 class HasherTest {
 
@@ -26,5 +27,14 @@ class HasherTest {
         val pwd = "a".repeat(1024 * 1024)
         val hashedPwd = Hasher.hash(pwd)
         assertTrue(Hasher.verify(pwd, hashedPwd).verified)
+    }
+
+    @DisplayName("Verifying very long almost identical password is not ok")
+    @Test
+    fun veryLongAlmostIdentical() {
+        val password = "a".repeat(1024 * 1024) + "a"
+        val almostPassword = "a".repeat(1024 * 1024) + "b"
+        val hashedPwd = Hasher.hash(password)
+        assertFalse(Hasher.verify(almostPassword, hashedPwd).verified)
     }
 }

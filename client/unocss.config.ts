dba3c36658f5e7ff4be52468b0045f832e0beb97
notKamui <teillard.jimmy@outlook.fr>
2023-02-02 01:44:28 +0100

    Added support for UnoCSS

---
+++ client/unocss.config.ts
@@ -0,0 +1,39 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [
+    ['btn', 'px-4 py-1 rounded inline-block bg-violet-700 text-white cursor-pointer hover:bg-violet-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-violet-600'],
+  ],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+      warn: true,
+    }),
+    presetTypography(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        display: 'Red Hat Display',
+        text: 'Red Hat Text',
+        mono: 'JetBrains Mono',
+      },
+    }),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
+  safelist: 'prose prose-sm m-auto text-left'.split(' '),
+})

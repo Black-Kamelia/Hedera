ef9d2cf855b14a596a4f842833be01199523d825
notKamui <teillard.jimmy@outlook.fr>
2023-02-02 14:54:26 +0100

    Added i18n support

---
+++ client/plugins/i18n.ts
@@ -0,0 +1,16 @@
+import { createI18n } from 'vue-i18n'
+
+const localesFolder = '../locales/'
+const locales = Object.entries(import.meta.glob('../locales/*.y(a)?ml', { eager: true }))
+const messages = Object.fromEntries(locales.map(([key, value]) => {
+  const yaml = key.endsWith('.yaml')
+  return [key.slice(localesFolder.length, yaml ? -5 : -4), (value as any).default]
+}))
+
+export default defineNuxtPlugin(({ vueApp }) => {
+  vueApp.use(createI18n({
+    legacy: false,
+    locale: 'en',
+    messages,
+  }))
+})

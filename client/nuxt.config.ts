141ea0289ab76aa854c214d38f1d0ec5d4d97ecd
notKamui <teillard.jimmy@outlook.fr>
2023-02-02 14:54:26 +0100

    Added i18n support

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,4 +1,7 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'url'
 import { transformShortVmodel } from '@vue-macros/short-vmodel'
+import VueI18nVitePlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -16,6 +19,13 @@ export default defineNuxtConfig({
     compilerOptions: {
       nodeTransforms: [transformShortVmodel({ prefix: '::' })],
     },
+  },
+  vite: {
+    plugins: [
+      VueI18nVitePlugin({
+        include: [resolve(dirname(fileURLToPath(import.meta.url)), 'locales/**')],
+      }),
+    ],
   },
   imports: {
     dirs: ['stores'],
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -4,15 +4,25 @@ const dark = $isDark
 
 <template>
   <div center h-screen>
-    <button class="icon-btn absolute top-10 right-10 text-8" @click="toggleDark()">
-      <div i="tabler-sun dark:tabler-moon" />
-    </button>
+    <div class="absolute top-10 right-10 flex flex-row gap-2">
+      <select class="px-2 py-1 rounded-lg" ::="$i18n.locale">
+        <option value="en">
+          en
+        </option>
+        <option value="fr">
+          fr
+        </option>
+      </select>
+      <button class="icon-btn text-8" @click="toggleDark()">
+        <div i="tabler-sun dark:tabler-moon" />
+      </button>
+    </div>
     <p class="absolute top-10 left-10 text-8 font-text">
       {{ dark ? 'Dark' : 'Light' }}
     </p>
 
     <h1 font="display bold" text="30 hover:violet-700" transition>
-      Welcome to Hedera
+      {{ $t('main.title') }}
     </h1>
   </div>
 </template>

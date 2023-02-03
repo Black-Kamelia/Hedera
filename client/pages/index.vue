cd49c6a6a934a81e96fe11ce0e00a4908852007a
notKamui <teillard.jimmy@outlook.fr>
2023-02-03 20:21:19 +0100

    Fixed dark useDark

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
+const { toggle } = useDark()
 const { locale } = useI18n()
-const dark = $isDark
 </script>
 
 <template>
-  <div center h-screen>
+  <div flex-center h-screen>
     <div class="absolute top-10 right-10 flex flex-row gap-2">
       <select v-model="locale" class="px-2 py-1 rounded-lg">
         <option value="en">
@@ -14,15 +14,15 @@ const dark = $isDark
           fr
         </option>
       </select>
-      <button class="icon-btn text-8" @click="toggleDark()">
+      <button class="icon-btn text-8" @click="toggle()">
         <div i="tabler-sun dark:tabler-moon" />
       </button>
     </div>
     <p class="absolute top-10 left-10 text-8 font-text">
-      {{ dark ? $t('theme.dark') : $t('theme.light') }}
+      {{ $colorMode.value === 'dark' ? $t('theme.dark') : $t('theme.light') }}
     </p>
 
-    <h1 font="display bold" text="30 hover:violet-700" transition>
+    <h1 font="display bold" text="30 hover:violet-500 dark:hover:emerald-500" transition>
       {{ $t('main.title') }}
     </h1>
   </div>

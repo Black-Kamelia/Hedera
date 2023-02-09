a1c8619175f92ce1c93259aa23a90d95540d01a0
Irwin Madet <irwin.madet@gmail.com>
2023-02-09 16:54:16 +0100

    Nice work, super easy 🤙

--- client/app.vue
+++ client/app.vue
@@ -11,4 +11,17 @@
 .p-component {
   font-family: "Red Hat Text", sans-serif;
 }
+
+.p-input-icon-left > i,
+.p-input-icon-right > i {
+  margin-top: -12px;
+}
+
+.p-input-icon-left > .p-inputtext {
+    padding-left: calc(1.25rem + 24px);
+}
+
+.p-input-icon-right > .p-inputtext {
+    padding-right: calc(1.25rem + 24px);
+}
 </style>

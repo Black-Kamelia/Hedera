fd62030fbab13e618067200794e9f30c9e21c2eb
Irwin Madet <irwin.madet@gmail.com>
2023-02-09 16:54:16 +0100

    Nice work, super easy 🤙

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -17,10 +17,16 @@ useHead({
 
   <div>
     <label for="email1" class="block font-900 font-medium mb-2">Email</label>
-    <PInputText id="email1" type="text" placeholder="john.doe@example.fr" class="w-full mb-3" />
+    <span class="p-input-icon-left mb-3">
+      <i class="i-tabler-mail" />
+      <PInputText id="email1" type="text" placeholder="john.doe@example.fr" class="w-full" />
+    </span>
 
     <label for="password1" class="block font-900 font-medium mb-2">Password</label>
-    <PInputText id="password1" type="password" placeholder="••••••••••••••••" class="w-full mb-3" />
+    <span class="p-input-icon-left mb-3">
+      <i class="i-tabler-lock" />
+      <PInputText id="password1" type="password" placeholder="••••••••••••••••" class="w-full" />
+    </span>
 
     <div class="flex flex-row items-center justify-between mb-6 w-100%">
       <div v-tooltip.bottom="{ value: 'Coming soon', class: 'p-0' }" class="flex flex-row items-center">
@@ -30,6 +36,6 @@ useHead({
       <a class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot password?</a>
     </div>
 
-    <PButton label="Sign In" icon="pi pi-user" class="w-full" />
+    <PButton label="Sign In" class="w-full" />
   </div>
 </template>

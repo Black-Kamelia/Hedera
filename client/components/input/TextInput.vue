cbd2bd381f096294e4ab139d53763625d8095f30
Irwin Madet <irwin.madet@gmail.com>
2023-04-04 00:04:15 +0200

    login page WIP

---
+++ client/components/input/TextInput.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useField } from 'vee-validate'
+import InputTextProps from 'primevue/inputtext'
+
+const props = defineProps({
+  ...InputTextProps,
+})
+
+const { errorMessage, value } = useField(props.name)
+</script>
+
+<template>
+  <PInputText v-bind="$attrs" v-model="value" :class="{ 'p-invalid': errorMessage }" />
+</template>

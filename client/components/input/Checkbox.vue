01631ca7f43120ec438442ff0842d8bc0d9588d1
notKamui <teillard.jimmy@outlook.fr>
2023-04-09 17:29:42 +0200

    Fixed OTP

--- client/components/input/Checkbox.vue
+++ client/components/input/Checkbox.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useField } from 'vee-validate'
 import type _CheckboxProps from 'primevue/checkbox'
+import PCheckbox from 'primevue/checkbox'
 
 interface CheckboxProps extends _CheckboxProps {
   name: string
@@ -10,11 +11,16 @@ interface CheckboxProps extends _CheckboxProps {
 const props = defineProps<CheckboxProps>()
 
 const { value } = useField<boolean>(props.name)
+
+const el = ref<Nullable<CompElement<InstanceType<typeof PCheckbox>>>>()
+defineExpose({
+  $el: computed(() => el.value?.$el),
+})
 </script>
 
 <template>
   <div class="flex flex-row items-center">
-    <PCheckbox :id="props.name" v-bind="$attrs" v-model="value" class="mr-2" />
+    <PCheckbox :id="props.name" ref="el" v-bind="$attrs" v-model="value" class="mr-2" />
     <label :for="props.name" class="text-gray">{{ props.label }}</label>
   </div>
 </template>

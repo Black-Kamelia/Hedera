afc86738035daf146b4adeb4cc1db4a804f41ece
notKamui <teillard.jimmy@outlook.fr>
2023-04-12 16:46:43 +0200

    I did the thing

--- client/utils/s.ts
+++ client/utils/s.ts
@@ -1,24 +1,17 @@
-import type {
-  StateTree,
-  StoreDefinition,
-  _ActionsTree,
-  _GettersTree,
-} from '@pinia/nuxt/dist/runtime/composables'
+import type { StoreDefinition } from '@pinia/nuxt/dist/runtime/composables'
 
 /**
  * Wraps a useStore composable with a new composable which returns a tuple of the store and its refs.
  * @param useStore The useStore composable to wrap.
  * @returns A new composable which returns a tuple of the store and its refs. ([refs, store])
  */
-export default function s<
-  Id extends string,
-  S extends StateTree = {},
-  G extends _GettersTree<S> = {},
-  A extends _ActionsTree = {},
->(useStore: StoreDefinition<Id, S, G, A>) {
-  return function useDestructurableStore() {
+export default function s<R>(useStore: StoreDefinition<any, any, any, any>): () => R {
+  return function useDestructurableStore(): R {
     const store = useStore()
     const refs = storeToRefs(store)
-    return [refs, store] as const
+    return {
+      ...store,
+      ...refs,
+    } as R
   }
 }

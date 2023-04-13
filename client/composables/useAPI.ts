d3cd4210d3e792055677791a8dc8865d0e899b51
notKamui <teillard.jimmy@outlook.fr>
2023-04-13 21:42:26 +0200

    Allowed for generic axios configuration

--- client/composables/useAPI.ts
+++ client/composables/useAPI.ts
@@ -1,14 +1,17 @@
 import type { StrictUseAxiosReturn, UseAxiosOptions } from '@vueuse/integrations/useAxios'
 import { useAxios } from '@vueuse/integrations/useAxios'
+import _axios from 'axios'
 import type { AxiosResponse, RawAxiosRequestConfig } from 'axios'
+
+const appConfig = useRuntimeConfig()
+const axios = _axios.create({
+  baseURL: appConfig.public.apiBaseUrl,
+})
 
 export default function useAPI<T = any, R = AxiosResponse<T>, D = any>(
   url: string,
   config?: RawAxiosRequestConfig<D>,
   options?: UseAxiosOptions<T>,
 ): StrictUseAxiosReturn<T, R, D> & PromiseLike<StrictUseAxiosReturn<T, R, D>> {
-  const appConfig = useRuntimeConfig()
-  if (!options)
-    return useAxios(url, { ...config, baseURL: appConfig.public.apiBaseUrl })
-  return useAxios(url, { ...config, baseURL: appConfig.public.apiBaseUrl }, options)
+  return useAxios(url, config ?? {}, axios, options ?? {})
 }

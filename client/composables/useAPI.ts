54e71f1ea12a78d3bddf4336fce84f94bd7edb8f
notKamui <teillard.jimmy@outlook.fr>
2023-04-13 23:56:16 +0200

    complete strong typing of axios middlewares

--- client/composables/useAPI.ts
+++ client/composables/useAPI.ts
@@ -1,12 +1,22 @@
 import type { StrictUseAxiosReturn, UseAxiosOptions } from '@vueuse/integrations/useAxios'
 import { useAxios } from '@vueuse/integrations/useAxios'
 import _axios from 'axios'
-import type { AxiosResponse, RawAxiosRequestConfig } from 'axios'
+import type { AxiosResponse, InternalAxiosRequestConfig, RawAxiosRequestConfig } from 'axios'
 
 const appConfig = useRuntimeConfig()
 const axios = _axios.create({
   baseURL: appConfig.public.apiBaseUrl,
 })
+
+axiosMiddlewares.requestMiddlewares.forEach(createInterceptorFactory(
+  axios.interceptors.request,
+  (config: InternalAxiosRequestConfig<any>) => config.url ?? '',
+))
+
+axiosMiddlewares.responseMiddlewares.forEach(createInterceptorFactory(
+  axios.interceptors.response,
+  (response: AxiosResponse<any, any>) => response.config.url ?? '',
+))
 
 export default function useAPI<T = any, R = AxiosResponse<T>, D = any>(
   url: string,
---
+++ client/utils/axios/types.ts
@@ -0,0 +1,57 @@
+import type { AxiosInterceptorManager, AxiosInterceptorOptions, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
+
+export interface AxiosMiddleware<V> {
+  route: string | RegExp | ((url: string) => boolean)
+  onFulfilled?: ((value: V) => V | Promise<V>) | null
+  onRejected?: ((error: any) => any) | null
+  options?: AxiosInterceptorOptions
+}
+
+export type AxiosRequestMiddleware<D> = AxiosMiddleware<InternalAxiosRequestConfig<D>>
+
+export type AxiosResponseMiddleware<T, D> = AxiosMiddleware<AxiosResponse<T, D>>
+
+export interface AxiosMiddlewares {
+  requestMiddlewares: AxiosRequestMiddleware<any>[]
+  responseMiddlewares: AxiosResponseMiddleware<any, any>[]
+}
+
+function checkRoute(route: string | RegExp | ((url: string) => boolean), url: string): boolean {
+  if (typeof route === 'string')
+    return route === url
+
+  if (route instanceof RegExp)
+    return route.test(url)
+
+  if (typeof route === 'function')
+    return route(url)
+
+  return false
+}
+
+export function createInterceptorFactory<V, T extends AxiosMiddleware<V>>(
+  axiosInterceptorManager: AxiosInterceptorManager<V>,
+  routerMapper: (input: V) => string,
+): (middleware: T) => void {
+  return function (middleware: T) {
+    let passes = false
+
+    axiosInterceptorManager.use(
+      (input) => {
+        if (checkRoute(middleware.route, routerMapper(input))) {
+          passes = true
+          middleware.onFulfilled?.(input)
+        }
+
+        return input
+      },
+      (error) => {
+        if (passes)
+          middleware.onRejected?.(error)
+
+        return Promise.reject(error)
+      },
+      middleware.options,
+    )
+  }
+}

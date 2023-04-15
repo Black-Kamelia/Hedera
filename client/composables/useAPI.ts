c5d2d33cafcf60c2382956abbfbd39eb5d21896a
notKamui <teillard.jimmy@outlook.fr>
2023-04-15 22:13:01 +0200

    I guess i actually did the thing

--- client/composables/useAPI.ts
+++ client/composables/useAPI.ts
@@ -4,7 +4,7 @@ import type { AxiosResponse, RawAxiosRequestConfig } from 'axios'
 
 export default function useAPI<T = any, R = AxiosResponse<T>, D = any>(
   url: string,
-  config?: RawAxiosRequestConfig<D>,
+  config?: RawAxiosRequestConfig<D> & { skipAuthRefresh?: boolean },
   options?: UseAxiosOptions<T>,
 ): StrictUseAxiosReturn<T, R, D> & PromiseLike<StrictUseAxiosReturn<T, R, D>> {
   const axiosInstance = useAxiosInstance()

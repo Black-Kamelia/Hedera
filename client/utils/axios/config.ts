53c9ac58a8c337812cf482cd6caeaa59f33ed7c6
notKamui <teillard.jimmy@outlook.fr>
2023-04-15 15:29:13 +0200

    Finalized general config of interceptors

--- client/utils/axios/config.ts
+++ client/utils/axios/config.ts
@@ -1,4 +1,4 @@
-import type { AxiosResponse, InternalAxiosRequestConfig } from 'axios'
+import type { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
 import axios from 'axios'
 
 const appConfig = useRuntimeConfig()
@@ -9,9 +9,11 @@ export const axiosInstance = axios.create({
 axiosMiddlewares.requestMiddlewares.forEach(createInterceptorFactory(
   axiosInstance.interceptors.request,
   (config: InternalAxiosRequestConfig<any>) => config.url ?? '',
+  () => '', // Well, as of now, errors on request interceptor can never happen, so this is ignorable
 ))
 
 axiosMiddlewares.responseMiddlewares.forEach(createInterceptorFactory(
   axiosInstance.interceptors.response,
   (response: AxiosResponse<any, any>) => response.config.url ?? '',
+  (error: AxiosError) => error.response?.config.url ?? '',
 ))
--- client/utils/axios/middlewares.ts
+++ client/utils/axios/middlewares.ts
@@ -2,12 +2,7 @@ import type { AxiosMiddlewares } from './types'
 
 export const axiosMiddlewares: AxiosMiddlewares = {
   requestMiddlewares: [
-    {
-      route: /.*/,
-      onRejected: (error) => {
-        console.log('error 2', error)
-      },
-    },
   ],
-  responseMiddlewares: [],
+  responseMiddlewares: [
+  ],
 }

1ca00f6aa358eebd074f19d4e4037bb1a69d3df2
Irwin Madet <irwin.madet@gmail.com>
2023-04-17 00:25:45 +0200

    Update existing i18n strings

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -3,7 +3,7 @@ import { useForm } from 'vee-validate'
 import { object, string } from 'yup'
 import { getRandomDeveloperName } from '~/utils/developerNames'
 
-const { t } = useI18n()
+const { t, e } = useI18n()
 
 definePageMeta({
   layout: 'centercard',
@@ -13,8 +13,8 @@ useHead({
 })
 
 const schema = object({
-  username: string().required(t('pages.login.form.errors.missing_username')),
-  password: string().required(t('pages.login.form.errors.missing_password')),
+  username: string().required(t('forms.login.errors.missing_username')),
+  password: string().required(t('forms.login.errors.missing_password')),
 })
 const { handleSubmit, errors, resetField } = useForm({
   validationSchema: schema,
@@ -27,17 +27,17 @@ onMounted(() => {
 })
 
 const usernamePlaceholder = getRandomDeveloperName()
-const showErrorMessage = ref(false)
+const errorMessage = ref<Nullable<String>>(null)
 
 function hideErrorMessage() {
-  showErrorMessage.value = false
+  errorMessage.value = null
 }
 
 const onSubmit = handleSubmit((values) => {
   login(values).catch((err) => {
     if (err.response?.status === 401) {
       resetField('password')
-      showErrorMessage.value = true
+      errorMessage.value = e(err.response?.data ?? 'errors.unknown')
     }
   })
 })
@@ -45,38 +45,36 @@ const onSubmit = handleSubmit((values) => {
 
 <template>
   <div class="text-center mb-10">
-    <h1 class="font-extrabold text-4xl mb-1">
+    <h1 class="font-extrabold text-5xl mb-1">
       Hedera
     </h1>
-    <h2 class="font-extrabold text-2xl mb-3">
+    <h2 class="font-extrabold text-3xl mb-3">
       {{ t('pages.login.title') }}
     </h2>
   </div>
 
-  <PMessage v-show="showErrorMessage" severity="error" icon="i-tabler-alert-circle-filled" :closable="false">
-    {{ t('pages.login.errors.invalid_credentials') }}
+  <PMessage v-show="errorMessage" severity="error" icon="i-tabler-alert-circle-filled" :closable="false">
+    {{ errorMessage }}
   </PMessage>
 
   <form @submit="onSubmit">
-    <label for="email1" class="block font-900 font-medium mb-2">{{ t('pages.login.form.fields.username') }}</label>
+    <label for="email1" class="block font-900 font-medium mb-2">{{ t('forms.login.fields.username') }}</label>
     <div class="mb-3">
       <span class="p-input-icon-left w-full">
         <i class="i-tabler-user" />
         <InputText
-          name="username" type="text" :placeholder="usernamePlaceholder" class="w-full"
-          @input="hideErrorMessage"
+          name="username" type="text" :placeholder="usernamePlaceholder" class="w-full" @input="hideErrorMessage"
         />
       </span>
       <small v-if="errors.username" id="text-error" class="p-error mt-1">{{ errors.username }}</small>
     </div>
 
-    <label for="password1" class="block font-900 font-medium mb-2">{{ t('pages.login.form.fields.password') }}</label>
+    <label for="password1" class="block font-900 font-medium mb-2">{{ t('forms.login.fields.password') }}</label>
     <div class="mb-3">
       <span class="p-input-icon-left w-full">
         <i class="i-tabler-lock" />
         <InputText
-          name="password" type="password" placeholder="••••••••••••••••" class="w-full"
-          @input="hideErrorMessage"
+          name="password" type="password" placeholder="••••••••••••••••" class="w-full" @input="hideErrorMessage"
         />
       </span>
       <small v-if="errors.password" id="text-error" class="p-error mt-1">{{ errors.password }}</small>
@@ -94,6 +92,6 @@ const onSubmit = handleSubmit((values) => {
       </a>
     </div>
 
-    <PButton :label="t('pages.login.form.submit')" class="w-full" type="submit" />
+    <PButton :label="t('forms.login.submit')" class="w-full" type="submit" />
   </form>
 </template>

d72492e17a4b37b1bf7bcd058443039140abc17c
notKamui <teillard.jimmy@outlook.fr>
2023-04-19 11:16:58 +0200

    Added utility extensions for defining event listeners and closing sessions

--- server/src/main/kotlin/com/kamelia/hedera/websocket/Routes.kt
+++ server/src/main/kotlin/com/kamelia/hedera/websocket/Routes.kt
@@ -5,11 +5,11 @@ import com.kamelia.hedera.core.Response
 import com.kamelia.hedera.core.respondNoError
 import com.kamelia.hedera.plugins.AuthJwt
 import com.kamelia.hedera.util.authenticatedUser
+import com.kamelia.hedera.util.forcefullyClose
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
-import io.ktor.websocket.*
 
 fun Route.webSocketRoutes() = route("/ws") {
     authenticate(AuthJwt) {
@@ -27,10 +27,10 @@ private fun Route.getWebSocketToken() = get("/token") {
 
 private fun Route.socketRoute() = webSocket("/") {
     val token = call.request.queryParameters["token"]
-        ?: return@webSocket close(CloseReason(CloseReason.Codes.VIOLATED_POLICY, "No token"))
+        ?: return@webSocket forcefullyClose("Missing token")
 
     val userId = validateToken(token)
-        ?: return@webSocket close(CloseReason(CloseReason.Codes.VIOLATED_POLICY, "Invalid or expired token"))
+        ?: return@webSocket forcefullyClose("Invalid or expired token")
 
     handleSession(userId)
 }

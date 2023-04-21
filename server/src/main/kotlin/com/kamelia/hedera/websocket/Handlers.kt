78bc5fee5df6daa370b8aac28945ce4ee0c51d64
notKamui <teillard.jimmy@outlook.fr>
2023-04-21 10:59:31 +0200

    simplified event listener definition

--- server/src/main/kotlin/com/kamelia/hedera/websocket/Handlers.kt
+++ server/src/main/kotlin/com/kamelia/hedera/websocket/Handlers.kt
@@ -7,26 +7,21 @@ import com.kamelia.hedera.util.gracefullyClose
 import com.kamelia.hedera.util.sendEvent
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
-import java.awt.SystemColor.text
 import java.util.*
 
-suspend fun WebSocketServerSession.handleSession(userId: UUID) {
-    val closers = mutableListOf<() -> Unit>()
-
-    // Define listeners here
-    closers += defineEventListener(UserEvents.userUpdatedEvent) { onUserUpdate(userId, it) }
-
-    keepAlive(closers)
-}
+suspend fun WebSocketServerSession.handleSession(userId: UUID) = keepAlive(
+    // Define event listeners here
+    defineEventListener(UserEvents.userUpdatedEvent) { onUserUpdate(userId, it) },
+)
 
 private const val USER_UPDATED = "user-updated"
 private suspend fun WebSocketServerSession.onUserUpdate(currentId: UUID, user: UserRepresentationDTO) {
     if (user.id != currentId) return
-    sendEvent<UserRepresentationDTO>(USER_UPDATED, user)
+    sendEvent(USER_UPDATED, user)
 }
 
 private const val CONNECTION_CLOSED_BY_CLIENT = "connection-closed-by-client"
-private suspend fun WebSocketServerSession.keepAlive(closers: List<() -> Unit>) {
+private suspend fun WebSocketServerSession.keepAlive(vararg closers: () -> Unit) {
     for (frame in incoming) {
         if (frame !is Frame.Text) continue
         val text = frame.readText()

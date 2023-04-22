ea02261cadd8f687508114f5415c19d5438289f0
Irwin Madet <irwin.madet@gmail.com>
2023-04-22 23:27:00 +0200

    Fix translations

--- client/pages/upload.vue
+++ client/pages/upload.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 const { t } = useI18n()
 
-usePageName(t('pages.upload.page_name'))
+usePageName(t('pages.upload.title'))
 definePageMeta({
   layout: 'sidebar',
   middleware: ['auth'],

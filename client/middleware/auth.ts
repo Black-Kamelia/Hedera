4acaf048f3feed890cfbbb40656908bc65b01356
notKamui <teillard.jimmy@outlook.fr>
2023-04-22 02:24:08 +0200

    simplified auth middleware

--- client/middleware/auth.ts
+++ client/middleware/auth.ts
@@ -1,5 +1,13 @@
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware((to) => {
   const { isAuthenticated } = useAuth()
-  if (!isAuthenticated.value)
-    return navigateTo('/login')
+
+  if ((to.path === '/login' || to.path === '/register')) {
+    if (isAuthenticated.value)
+      return navigateTo('/files', { replace: true })
+  }
+  else if (!isAuthenticated.value) {
+    return navigateTo('/login', { replace: true })
+  }
+
+  // continue
 })

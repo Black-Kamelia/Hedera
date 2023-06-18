8ba51632e85c8368f691bb4addd68fed0b39e1bc
Irwin Madet <irwin.madet@gmail.com>
2023-06-18 14:33:21 +0200

    Move background to layout instead of page + add parallax effect

--- client/app.vue
+++ client/app.vue
@@ -37,7 +37,7 @@ useEventBus(LoggedOutEvent).on(() => {
   </div>
 </template>
 
-<style>
+<style lang="scss">
 .layout-in-enter-active,
 .layout-in-leave-active {
   position: absolute;
@@ -46,16 +46,28 @@ useEventBus(LoggedOutEvent).on(() => {
   width: 100%;
   height: 100%;
   transition: all 0.4s cubic-bezier(0.87, 0, 0.13, 1);
+
+  .p-card {
+    transition: all 0.4s cubic-bezier(0.87, 0, 0.13, 1);
+  }
 }
+
+/* MAIN */
 .layout-in-enter-from {
   opacity: 0;
   transform: scale(0.95);
-  filter: blur(5px);
+  z-index: 0;
 }
+/* LOGIN */
 .layout-in-leave-to {
   opacity: 0;
-  transform: scale(1.15);
+  transform: scale(1.1);
   filter: blur(10px);
+  z-index: 10;
+
+  .p-card {
+    transform: scale(1.2);
+  }
 }
 
 .layout-out-enter-active,
@@ -65,16 +77,28 @@ useEventBus(LoggedOutEvent).on(() => {
   left: 0;
   width: 100%;
   height: 100%;
-  transition: all 0.4s cubic-bezier(0.87, 0, 0.13, 1);
+  transition: all 0.6s cubic-bezier(0.16, 1, 0.3, 1);
+
+  .p-card {
+    transition: all 0.6s cubic-bezier(0.16, 1, 0.3, 1);
+  }
 }
+/* LOGIN */
 .layout-out-enter-from {
   opacity: 0;
-  transform: scale(1.15);
+  transform: scale(1.1);
   filter: blur(10px);
+  z-index: 10;
+
+  .p-card {
+    transform: scale(1.2);
+  }
 }
+/* MAIN */
 .layout-out-leave-to {
   opacity: 0;
   transform: scale(0.95);
-  filter: blur(5px);
+  z-index: 0;
+  //filter: blur(5px);
 }
 </style>

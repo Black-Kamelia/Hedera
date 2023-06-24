7a3dda7b5ae1d63d78821d6505088920293f6ea8
Irwin Madet <irwin.madet@gmail.com>
2023-06-24 20:42:13 +0200

    i did it (kinda)

--- client/layouts/sidebar.vue
+++ client/layouts/sidebar.vue
@@ -1,11 +1,24 @@
+<script lang="ts" setup>
+import { useStorage } from '@vueuse/core'
+
+const sidebar = toReactive(useStorage('sidebar', { open: true }))
+const sidebarWidth = computed(() => sidebar.open ? '19em' : '5em')
+</script>
+
 <template>
-  <div class="flex flex-row" h-screen w-screen>
+  <div class="flex flex-row overflow-auto" h-screen w-screen>
     <Sidebar />
-    <div class="flex-grow flex flex-col">
+    <div class="flex-grow flex flex-col content" :style="`max-width: calc(100% - ${sidebarWidth})`">
       <Topbar />
-      <div class="py-4 px-8 w-full h-full overflow-y-auto">
+      <div class="py-4 px-8 w-full h-full overflow-auto">
         <slot />
       </div>
     </div>
   </div>
 </template>
+
+<style>
+.content {
+  transition: max-width 0.3s ease, width 0.3s ease;
+}
+</style>
--- client/pages/debug/layout.vue
+++ client/pages/debug/layout.vue
@@ -7,7 +7,12 @@ definePageMeta({
 
 <template>
   <PButton icon="i-tabler-arrow-left" label="Back" @click="navigateTo('/debug')" />
-  <div class="p-5 w-100% h-100 bg-orange">
+  <div class="p-5 w-100% h-100 bg-red">
+    <h1 class="text-4xl font-bold">
+      W-100% H-100px
+    </h1>
+  </div>
+  <div class="p-5 w-200% h-100 bg-violet">
     <h1 class="text-4xl font-bold">
       W-100% H-100px
     </h1>
@@ -17,9 +22,14 @@ definePageMeta({
       W-100px H-100%
     </h1>
   </div>
-  <div class="p-5 w-150% h-50 bg-yellow">
+  <div class="p-5 w-screen h-100 bg-orange">
     <h1 class="text-4xl font-bold">
-      W-150% H-50px
+      W-screen H-100px
+    </h1>
+  </div>
+  <div class="p-5 w-100 h-screen bg-blue">
+    <h1 class="text-4xl font-bold">
+      W-100px H-screen
     </h1>
   </div>
 </template>

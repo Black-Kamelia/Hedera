e85618bd20b4cd75a73d03c55f90784215f2b91e
Irwin Madet <irwin.madet@gmail.com>
2023-06-26 22:28:21 +0200

    Add more components to debug

--- client/pages/debug/components.vue
+++ client/pages/debug/components.vue
@@ -8,13 +8,22 @@
         Buttons
       </h2>
       <div class="flex gap-2">
-        <PButton label="Primary" />
-        <PButton label="Secondary" severity="secondary" />
-        <PButton label="Success" severity="success" />
-        <PButton label="Warning" severity="warning" />
-        <PButton label="Danger" severity="danger" />
-        <PButton label="Info" severity="info" />
-        <PButton label="Help" severity="help" />
+        <PButton size="small" label="Primary" />
+        <PButton size="small" label="Secondary" severity="secondary" />
+        <PButton size="small" label="Success" severity="success" />
+        <PButton size="small" label="Warning" severity="warning" />
+        <PButton size="small" label="Danger" severity="danger" />
+        <PButton size="small" label="Info" severity="info" />
+        <PButton size="small" label="Help" severity="help" />
+      </div>
+      <div class="flex gap-2">
+        <PSplitButton size="small" label="Primary" icon="pi pi-plus" :model="[]" />
+        <PSplitButton size="small" label="Secondary" icon="pi pi-plus" severity="secondary" :model="[]" />
+        <PSplitButton size="small" label="Success" icon="pi pi-plus" severity="success" :model="[]" />
+        <PSplitButton size="small" label="Warning" icon="pi pi-plus" severity="warning" :model="[]" />
+        <PSplitButton size="small" label="Danger" icon="pi pi-plus" severity="danger" :model="[]" />
+        <PSplitButton size="small" label="Info" icon="pi pi-plus" severity="info" :model="[]" />
+        <PSplitButton size="small" label="Help" icon="pi pi-plus" severity="help" :model="[]" />
       </div>
     </div>
 

810bd634cd6b81b13ce954bbc292b06aa47b5552
Irwin Madet <irwin.madet@gmail.com>
2023-06-27 14:49:27 +0200

    Clean minify script

--- client/scripts/css-minify.cjs
+++ client/scripts/css-minify.cjs
@@ -1,10 +1,11 @@
 const fs = require('node:fs');
 const CleanCSS = require("clean-css");
 
-function minify(input, output) {
-  const minifiedCode = new CleanCSS().minify([input]);
-  fs.writeFileSync(output, minifiedCode.styles);
+const instance = new CleanCSS();
 
+function minify(input, output) {
+  const minifiedCode = instance.minify([input]);
+  fs.writeFileSync(output, minifiedCode.styles);
 }
 
 minify('.output/public/assets/css/light-theme.css', '.output/public/assets/css/light-theme.css')

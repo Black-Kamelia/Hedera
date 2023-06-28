88b73a49ae12aedcb9771b8cbe62565c999787e9
Irwin Madet <irwin.madet@gmail.com>
2023-06-28 16:11:58 +0200

    Tweak 404 page (wip)

--- client/pages/[...all].vue
+++ client/pages/[...all].vue
@@ -1,3 +1,18 @@
+<script setup>
+const { isAuthenticated } = useAuth()
+
+watch(isAuthenticated, (val) => {
+  setPageLayout(val ? 'sidebar' : 'centercard')
+  usePageName('404')
+}, { immediate: true })
+
+// definePageMeta({ layout })
+</script>
+
 <template>
-  <p>404 frr</p>
+  <div class="h-full w-full flex justify-center items-center">
+    <h1 class="text-5xl">
+      404
+    </h1>
+  </div>
 </template>

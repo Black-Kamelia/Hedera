3efcac48a999c2a5f755d4fc858eead01aea6b4c
Irwin Madet <irwin.madet@gmail.com>
2023-07-01 18:12:38 +0200

    Update client/components/ui/files/VisibilityDisplayer.vue
    Co-authored-by: notKamui <46132319+notKamui@users.noreply.github.com>

--- client/components/ui/files/VisibilityDisplayer.vue
+++ client/components/ui/files/VisibilityDisplayer.vue
@@ -1,29 +1,23 @@
 <script lang="ts" setup>
+import type { FileVisibility } from '~/composables/fileTable/useChangeFileVisibility'
+
 const { visibility } = defineProps<{
-  visibility: string
+  visibility: FileVisibility
 }>()
 const { t } = useI18n()
+
+const mapper = computed(() => ({
+  PUBLIC: { icon: 'i-tabler-world', text: t('pages.files.visibility.public') },
+  UNLISTED: { icon: 'i-tabler-link', text: t('pages.files.visibility.unlisted') },
+  PROTECTED: { icon: 'i-tabler-lock', text: t('pages.files.visibility.protected') },
+  PRIVATE: { icon: 'i-tabler-eye-off', text: t('pages.files.visibility.private') },
+  UNKNOWN: { icon: 'i-tabler-help-triangle-filled', text: t('pages.files.visibility.unknown') },
+})
 </script>
 
 <template>
-  <div v-if="visibility === 'PUBLIC'" class="flex flex-row items-center gap-2">
-    <i class="i-tabler-world" />
-    <span>{{ t('pages.files.visibility.public') }}</span>
-  </div>
-  <div v-else-if="visibility === 'UNLISTED'" class="flex flex-row items-center gap-2">
-    <i class="i-tabler-link" />
-    <span>{{ t('pages.files.visibility.unlisted') }}</span>
-  </div>
-  <div v-else-if="visibility === 'PROTECTED'" class="flex flex-row items-center gap-2">
-    <i class="i-tabler-lock" />
-    <span>{{ t('pages.files.visibility.protected') }}</span>
-  </div>
-  <div v-else-if="visibility === 'PRIVATE'" class="flex flex-row items-center gap-2">
-    <i class="i-tabler-eye-off" />
-    <span>{{ t('pages.files.visibility.private') }}</span>
-  </div>
-  <div v-else class="flex flex-row items-center gap-2">
-    <i class="i-tabler-help-triangle-filled" />
-    <span>{{ t('pages.files.visibility.unknown') }}</span>
+  <div class="flex flex-row items-center gap-2">
+    <i :class="mapper[visibility].icon" />
+    <span>{{ mapper[visibility].text }}</span>
   </div>
 </template>

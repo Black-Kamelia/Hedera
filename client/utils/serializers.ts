5b943fcfbf97c81030bb5d10189820c2237407f3
notKamui <teillard.jimmy@outlook.fr>
2023-07-03 09:48:38 +0200

    regex go brrrr

--- client/utils/serializers.ts
+++ client/utils/serializers.ts
@@ -1,6 +1,5 @@
 function isISODate(value: string) {
-  const date = Date.parse(value)
-  return !Number.isNaN(date) && new Date(date).toISOString() === value
+  return /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$/.test(value)
 }
 
 export const jsonDateSerializer = {

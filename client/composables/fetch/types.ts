4978386233065f6340affe198a37e9b5c12114c9
notKamui <teillard.jimmy@outlook.fr>
2023-07-04 21:02:41 +0200

    optimized types

--- client/composables/fetch/types.ts
+++ client/composables/fetch/types.ts
@@ -9,19 +9,20 @@ export type UseFetchAPIOptions<T> = UseFetchOptions<T> & {
   ignoreAPIBaseURL?: boolean
 }
 
-export type OnRequestInterceptorFunction<T = any> = NonNullable<FetchAPIOptions['onRequest']> & NonNullable<UseFetchAPIOptions<T>['onRequest']>
-export type OnResponseInterceptorFunction<T = any> = NonNullable<FetchAPIOptions['onResponse']> & NonNullable<UseFetchAPIOptions<T>['onResponse']>
-export type OnRequestErrorInterceptorFunction<T = any> = NonNullable<FetchAPIOptions['onRequestError']> & NonNullable<UseFetchAPIOptions<T>['onRequestError']>
-export type OnResponseErrorInterceptorFunction<T = any> = NonNullable<FetchAPIOptions['onResponseError']> & NonNullable<UseFetchAPIOptions<T>['onResponseError']>
+export type OnRequestInterceptorFunction<T = any> = FetchAPIOptions['onRequest'] & NonNullable<UseFetchAPIOptions<T>['onRequest']>
+export type OnResponseInterceptorFunction<T = any> = FetchAPIOptions['onResponse'] & NonNullable<UseFetchAPIOptions<T>['onResponse']>
+export type OnRequestErrorInterceptorFunction<T = any> = FetchAPIOptions['onRequestError'] & NonNullable<UseFetchAPIOptions<T>['onRequestError']>
+export type OnResponseErrorInterceptorFunction<T = any> = FetchAPIOptions['onResponseError'] & NonNullable<UseFetchAPIOptions<T>['onResponseError']>
 
 export type InterceptorType = 'onRequest' | 'onResponse' | 'onRequestError' | 'onResponseError'
 
-export type InterceptorFunction<I extends InterceptorType, T = any> =
-  I extends 'onRequest' ? OnRequestInterceptorFunction<T> :
-    I extends 'onResponse' ? OnResponseInterceptorFunction<T> :
-      I extends 'onRequestError' ? OnRequestErrorInterceptorFunction<T> :
-        I extends 'onResponseError' ? OnResponseErrorInterceptorFunction<T> :
-          never
+interface InterceptorLookup<T = any> {
+  onRequest: OnRequestInterceptorFunction<T>
+  onResponse: OnResponseInterceptorFunction<T>
+  onRequestError: OnRequestErrorInterceptorFunction<T>
+  onResponseError: OnResponseErrorInterceptorFunction<T>
+}
+export type InterceptorFunction<I extends InterceptorType, T = any> = InterceptorLookup<T>[I]
 
 export type InterceptorRoute = string | string[] | RegExp | ((url: string) => boolean)
 

3fea22f049e4120b0ea925a63e42f8fd7764d38d
notKamui <teillard.jimmy@outlook.fr>
2023-07-15 00:24:17 +0200

    backend: upgrade dependencies (not liquibase because liquibase cringe)

--- server/src/main/kotlin/com/kamelia/hedera/util/Serializers.kt
+++ server/src/main/kotlin/com/kamelia/hedera/util/Serializers.kt
@@ -1,16 +1,12 @@
 package com.kamelia.hedera.util
 
-import java.util.UUID
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import java.util.*
 
-@OptIn(ExperimentalSerializationApi::class)
-@Serializer(forClass = UUID::class)
 object UUIDSerializer : KSerializer<UUID> {
 
     override val descriptor = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING)

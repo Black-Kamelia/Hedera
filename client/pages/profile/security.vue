7332ba13ba080ebdc232341a71e44d2934dfb79b
Irwin Madet <irwin.madet@gmail.com>
2023-07-26 19:21:17 +0200

    Remove commented out code

--- client/pages/profile/security.vue
+++ client/pages/profile/security.vue
@@ -4,23 +4,6 @@ const { t } = useI18n()
 
 <template>
   <div class="flex flex-col gap-3">
-    <!-- <div class="flex flex-col lg:flex-row lg:flex-grow gap-3"> -->
     <PasswordUpdater />
-    <!-- </div> -->
   </div>
-
-  <!-- <div class="p-card p-5">
-    <PMessage
-      severity="warn" :closable="false" :pt="{
-        root: { style: { margin: 0 } },
-      }"
-    >
-      <template #messageicon>
-        <i class="i-tabler-shield-lock-filled text-3xl mr-5" />
-      </template>
-      <h2 class="text-xl">
-        Certains paramètres de sécurité requièrent votre attention
-      </h2>
-    </PMessage>
-  </div> -->
 </template>

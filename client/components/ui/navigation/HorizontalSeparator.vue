8212c7f087ca953ee70b6da23786566ce0560839
Irwin Madet <irwin.madet@gmail.com>
2023-08-03 18:13:22 +0200

    Starting to rework top bar

---
+++ client/components/ui/navigation/HorizontalSeparator.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="px-4 w-full">
+    <div class="sep" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.sep {
+  width: 100%;
+  border-bottom: 1px solid var(--primary-400);
+
+  .dark & {
+    border-bottom: 1px solid var(--primary-700);
+  }
+}
+</style>
---
+++ client/components/ui/navigation/topbar/Topbar.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+export interface TopBarProps {
+  narrow?: boolean
+}
+
+const {
+  narrow,
+} = defineProps<TopBarProps>()
+
+const { logout } = useAuth()
+const pageName = usePageName()
+</script>
+
+<template>
+  <header
+    class="top-bar flex items-center overflow-hidden gap-4 h-5em w-full"
+    :class="{ 'px-8': !narrow, 'px-4': narrow }"
+  >
+    <h2 class="text-3xl w-full">
+      {{ pageName }}
+    </h2>
+    <!-- <QuotaPreviewer :quota="{ value: 10, shift: 20 }" :max="{ value: 20, shift: 20 }" :ratio="0.5" /> -->
+    <VerticalSeparator />
+    <div class="flex flex-row gap-2">
+      <!--
+      <ThemeSwitcher />
+      <PButton v-tooltip.bottom="{ value: 'Notification', showDelay: '1000' }" icon="i-tabler-bell" text rounded />
+      -->
+      <PButton
+        v-tooltip.bottom="{ value: 'Log out', showDelay: '1000' }" icon="i-tabler-logout" text rounded
+        @click="logout()"
+      />
+    </div>
+  </header>
+
+  <!--
+  <header class="top-bar flex items-center overflow-hidden overflow-x-auto gap-4 px-8 py-5 h-20 w-full bg-[var(--surface-overlay)]">
+    <h2 class="text-3xl w-full">
+      {{ pageName }}
+    </h2>
+    <QuotaPreviewer :quota="{ value: 10, shift: 20 }" :max="{ value: 20, shift: 20 }" :ratio="0.5" />
+    <TopbarSeparator />
+    <div class="flex flex-row gap-2">
+      <ThemeSwitcher />
+      <PButton v-tooltip.bottom="{ value: 'Notification', showDelay: '1000' }" icon="i-tabler-bell" text rounded />
+      <PButton v-tooltip.bottom="{ value: 'Log out', showDelay: '1000' }" icon="i-tabler-logout" text rounded @click="logout()" />
+    </div>
+  </header>
+  -->
+</template>
+
+<style scoped>
+.top-bar {
+  border-bottom: 1px solid var(--surface-border);
+  background-color: var(--surface-overlay);
+}
+</style>

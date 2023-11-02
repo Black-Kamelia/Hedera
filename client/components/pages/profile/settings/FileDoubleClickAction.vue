b967d3ba639a02c084d400de52cbfca3a315a485
Irwin Madet <irwin.madet@gmail.com>
2023-11-02 17:31:24 +0100

    Add tooltip

--- client/components/pages/profile/settings/FileDoubleClickAction.vue
+++ client/components/pages/profile/settings/FileDoubleClickAction.vue
@@ -9,13 +9,42 @@ const { t } = useI18n()
 const { patchSetting, isError } = useSetting(model, value => ({ fileDoubleClickAction: value }))
 
 const options = computed(() => [
-  { icon: 'i-tabler-external-link', name: t('pages.profile.settings.file_double_click_action.open_new_tab'), value: 'OPEN_NEW_TAB' },
-  { icon: 'i-tabler-eye', name: t('pages.profile.settings.file_double_click_action.open_preview'), value: 'OPEN_PREVIEW' },
-  { icon: 'i-tabler-link', name: t('pages.profile.settings.file_double_click_action.copy_link'), value: 'COPY_LINK' },
-  { icon: 'i-tabler-sparkles', name: t('pages.profile.settings.file_double_click_action.copy_custom_link'), value: 'COPY_CUSTOM_LINK' },
-  { icon: 'i-tabler-pencil', name: t('pages.profile.settings.file_double_click_action.rename'), value: 'RENAME_FILE' },
-  { icon: 'i-tabler-trash', name: t('pages.profile.settings.file_double_click_action.delete'), value: 'DELETE_FILE' },
-  { icon: 'i-tabler-download', name: t('pages.profile.settings.file_double_click_action.download'), value: 'DOWNLOAD_FILE' },
+  {
+    icon: 'i-tabler-external-link',
+    name: t('pages.profile.settings.file_double_click_action.open_new_tab'),
+    value: 'OPEN_NEW_TAB',
+  },
+  {
+    icon: 'i-tabler-eye',
+    name: t('pages.profile.settings.file_double_click_action.open_preview'),
+    value: 'OPEN_PREVIEW',
+  },
+  {
+    icon: 'i-tabler-link',
+    name: t('pages.profile.settings.file_double_click_action.copy_link'),
+    value: 'COPY_LINK',
+  },
+  {
+    icon: 'i-tabler-sparkles',
+    name: t('pages.profile.settings.file_double_click_action.copy_custom_link'),
+    value: 'COPY_CUSTOM_LINK',
+    tooltip: t('pages.profile.settings.file_double_click_action.copy_custom_link_tooltip'),
+  },
+  {
+    icon: 'i-tabler-pencil',
+    name: t('pages.profile.settings.file_double_click_action.rename'),
+    value: 'RENAME_FILE',
+  },
+  {
+    icon: 'i-tabler-trash',
+    name: t('pages.profile.settings.file_double_click_action.delete'),
+    value: 'DELETE_FILE',
+  },
+  {
+    icon: 'i-tabler-download',
+    name: t('pages.profile.settings.file_double_click_action.download'),
+    value: 'DOWNLOAD_FILE',
+  },
 ])
 
 function getOption(value: string) {
@@ -53,6 +82,14 @@ function getOption(value: string) {
         <div class="flex items-center gap-2">
           <i :class="slotProps.option.icon" />
           <div>{{ slotProps.option.name }}</div>
+          <i
+            v-if="slotProps.option.tooltip"
+            v-tooltip.left="{
+              value: slotProps.option.tooltip,
+              class: 'important-z-1002 min-w-15em',
+            }"
+            class="h-1em w-1em i-tabler-info-circle-filled"
+          />
         </div>
       </template>
     </PDropdown>

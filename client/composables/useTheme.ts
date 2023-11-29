95143e9b3154bd83f1344ae1acd991a1984e538e
Irwin Madet <irwin.madet@gmail.com>
2023-11-29 21:17:07 +0100

    Theme update WIP

--- client/composables/useTheme.ts
+++ client/composables/useTheme.ts
@@ -7,6 +7,10 @@ export default function useTheme() {
       link: [
         {
           rel: 'stylesheet',
+          href: '/assets/css/reset.css',
+        },
+        {
+          rel: 'stylesheet',
           href: theme.main,
         },
         {

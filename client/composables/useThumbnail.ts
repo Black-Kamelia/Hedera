bf5fb624e1ef164b9d026bb26bf50e29e6ffbe06
Irwin Madet <irwin.madet@gmail.com>
2024-01-05 18:40:28 +0100

    Rework useThumbnail

--- client/composables/useThumbnail.ts
+++ client/composables/useThumbnail.ts
@@ -1,6 +1,11 @@
-import { blobToBase64 } from '~/utils/blobs'
+import type { FetchError } from 'ofetch'
+import type { MaybeRef } from 'vue'
 
 const SUPPORTED_IMAGE_TYPES = ['image/png', 'image/jpeg', 'image/gif']
+const CACHE_LIMIT = 50
+
+const thumbnails: Record<string, string> = {}
+const queue: string[] = []
 
 /**
  * Composable for getting a thumbnail of a file. If the file type does not support thumbnails, the thumbnail will be null.
@@ -10,24 +15,45 @@ const SUPPORTED_IMAGE_TYPES = ['image/png', 'image/jpeg', 'image/gif']
  *
  * @returns Thumbnail, loading state and error state
  */
-export function useThumbnail(code: string, mimeType: string) {
-  if (SUPPORTED_IMAGE_TYPES.includes(mimeType)) {
-    const { data, pending, error } = useAsyncData(`${code}_preview`, () => {
-      return $fetchAPI<Blob>(`/files/${code}/thumbnail`, { responseType: 'blob' })
-        .then(response => blobToBase64(response))
-        .catch(() => null)
-    })
+export function useThumbnail(code: MaybeRef<string>, mimeType: MaybeRef<string>) {
+  const data = ref<string | null>(null)
+  const loading = ref(false)
+  const error = ref<FetchError | null>(null)
 
-    return {
-      thumbnail: data,
-      loading: readonly(pending),
-      error: readonly(error),
+  watchEffect((onCleanup) => {
+    const rawCode = unref(code)
+    const rawMimeType = unref(mimeType)
+
+    if (!SUPPORTED_IMAGE_TYPES.includes(rawMimeType)) /* mime type is not supported */ return
+
+    const thumbnail = thumbnails[rawCode]
+    if (thumbnail) {
+      data.value = thumbnail
+      return
     }
-  }
 
-  return {
-    thumbnail: ref(null),
-    loading: readonly(ref(false)),
-    error: readonly(ref(false)),
-  }
+    if (queue.length >= CACHE_LIMIT) {
+      const oldest = queue.pop()
+      if (oldest) delete thumbnails[oldest]
+    }
+
+    const abortController = new AbortController()
+    loading.value = true
+    $fetchAPI<Blob>(`/files/${rawCode}/thumbnail`, { responseType: 'blob', signal: abortController.signal })
+      .then(data => blobToBase64(data))
+      .then((base64) => {
+        thumbnails[rawCode] = base64
+        queue.unshift(rawCode)
+        data.value = base64
+      })
+      .catch((err) => {
+        error.value = err
+      })
+      .finally(() => {
+        loading.value = false
+      })
+    onCleanup(() => abortController.abort())
+  })
+
+  return { thumbnail: data, loading: readonly(loading), error: readonly(error) }
 }

bf22d71fae0e2efd531d1967abc9c3b20bb0282e
Irwin Madet <irwin.madet@gmail.com>
2024-01-07 00:57:58 +0100

    Update auth pages

--- client/pages/reset-password.vue
+++ client/pages/reset-password.vue
@@ -1,46 +1,39 @@
 <script setup lang="ts">
 const { t } = useI18n()
 
+type ResetPasswordState = 'REQUEST_TOKEN' | 'INPUT_TOKEN' | 'RESET_PASSWORD'
+
 usePageName(() => t('pages.reset_password.title'))
 definePageMeta({
   layout: 'auth',
   middleware: ['auth', 'card-transitions'],
 })
-/*
-import { object, string } from 'yup'
 
-interface ResetPasswordForm {
-  email: string
+const { currentRoute } = useRouter()
+const query = computed(() => currentRoute.value.query)
+const resetTokenParam = computed(() => {
+  if (Object.keys(query.value).includes('token')) {
+    return query.value.token as string
+  }
+  return null
+})
+
+const state = ref<ResetPasswordState>('REQUEST_TOKEN')
+const resetToken = ref('')
+const resetTokenMetadata = ref<ResetPasswordTokenDTO>()
+
+function onTokenValidated(token: string, dto: ResetPasswordTokenDTO) {
+  state.value = 'RESET_PASSWORD'
+  resetToken.value = token
+  resetTokenMetadata.value = dto
 }
 
-const { t } = useI18n()
-
-usePageName(() => t('pages.reset_password.title'))
-definePageMeta({
-  layout: 'centercard',
-  middleware: ['auth', 'card-transitions'],
-})
-
-const schema = object({
-  email: string()
-    .email(t('forms.reset_password.errors.invalid_email'))
-    .required(t('forms.reset_password.errors.missing_email')),
-})
-const { handleSubmit } = useForm<ResetPasswordForm>({
-  validationSchema: schema,
-})
-
-const usernamePlaceholder = getRandomDeveloperUsername()
-const showErrorMessage = ref(false)
-
-function hideErrorMessage() {
-  showErrorMessage.value = false
-}
-
-const onSubmit = handleSubmit((_) => {
-  // TODO: not implemented yet
-})
- */
+watch(resetTokenParam, (token) => {
+  if (token !== null) {
+    state.value = 'INPUT_TOKEN'
+    resetToken.value = token
+  }
+}, { immediate: true })
 </script>
 
 <template>
@@ -49,6 +42,24 @@ const onSubmit = handleSubmit((_) => {
       {{ t('pages.reset_password.title') }}
     </h2>
 
-    <ResetPasswordForm class="w-125" />
+    <SlideTransitionContainer>
+      <ResetPasswordRequestForm
+        v-if="state === 'REQUEST_TOKEN'"
+        class="w-125"
+        @completed="state = 'INPUT_TOKEN'"
+      />
+      <ResetPasswordTokenForm
+        v-if="state === 'INPUT_TOKEN'"
+        :token="resetToken"
+        class="w-125"
+        @completed="onTokenValidated"
+      />
+      <ResetPasswordForm
+        v-if="state === 'RESET_PASSWORD' && resetTokenMetadata"
+        class="w-125"
+        :token="resetToken"
+        :token-metadata="resetTokenMetadata"
+      />
+    </SlideTransitionContainer>
   </div>
 </template>

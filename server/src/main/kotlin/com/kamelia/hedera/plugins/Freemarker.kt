6f514b623174705b8011d334a76bccbdb3b613f1
Irwin Madet <irwin.madet@gmail.com>
2024-01-10 14:47:07 +0100

    Fix imports

--- server/src/main/kotlin/com/kamelia/hedera/plugins/Freemarker.kt
+++ server/src/main/kotlin/com/kamelia/hedera/plugins/Freemarker.kt
@@ -1,16 +1,10 @@
 package com.kamelia.hedera.plugins
 
-import freemarker.cache.*
+import freemarker.cache.ClassTemplateLoader
 import freemarker.template.Configuration
-import io.ktor.serialization.*
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import io.ktor.server.velocity.*
 import java.io.StringWriter
-import org.apache.velocity.runtime.RuntimeConstants
-import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader
 
 private lateinit var configuration: Configuration
 
